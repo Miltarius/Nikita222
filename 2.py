64db6be1934b17511cb9d6c2b5e80195122c4e89
Miltarius <90238349+Miltarius@users.noreply.github.com>
2021-10-11 20:20:26 +0300

    Add files via upload
    ДЗ 1

--- 2.py
+++ 2.py
@@ -3,8 +3,8 @@ b = int(input('Введите число b'))
 c = int(input('Введите число c'))
 d = int(input('Введите число d'))
 f = int(input('Введите число f'))
-x = (a*b-c)
-y = (f-d)
+x = (a * b - c)
+y = (f - d)
 if y == 0:
     print('Ошибка:Делить на ноль нельзя')
 else: 
