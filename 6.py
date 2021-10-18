dcaa69527079e6e89ef18f59c24af0ecb36e2761
Miltarius <90238349+Miltarius@users.noreply.github.com>
2021-10-18 20:42:43 +0300

    Add files via upload

--- 6.py
+++ 6.py
@@ -1,7 +1,7 @@
-a = int(input('Введи строку'))
+a = str(input('Введи строку'))
 k = int(input('Введите цифру'))
-s = []
+s = list()
 for i in a:
     if i.isdigit():
         s.append(i)
-print(k, '-ая цифра в строке', s[k - 1])
+print(k, '-ая цифра в строке', s[k-1])
