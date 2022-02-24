97dd44738ee00b32d16ef9dabc3bdf12e66afca6
Miltarius <90238349+Miltarius@users.noreply.github.com>
2022-02-24 19:43:33 +0300

    Add files via upload

---
+++ дз 1  дроби/1.py
@@ -0,0 +1,26 @@
+class Fraction: 
+        def __init__(self, top=0, bottom=1): 
+            self.num = top 
+            self.den = bottom 
+
+        def input(self): 
+            self.num = int(input("Введите числитель: "))         
+            self.den = int(input("Введите знаменатель: "))  
+            if self.den == 0: 
+                print ("error") 
+            else: 
+                self.den != 0 
+
+        def __str__(self): 
+            return str(self.num)+"/"+str(self.den) 
+
+a1 = Fraction() 
+print(a1) 
+ 
+a2 = Fraction(3, 4) 
+print(a2) 
+ 
+a3 = Fraction() 
+a3.input() 
+print(a3) 
+ 
