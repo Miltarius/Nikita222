a534226bbd382688d9667451a1044c393a7ec163
Miltarius <90238349+Miltarius@users.noreply.github.com>
2022-04-09 12:22:25 +0300

    Delete ДЗ4 directory

--- ДЗ4/2.py
+++
@@ -1,9 +0,0 @@
-def asd(n):
-    q = 1
-    for q in range(2, n + 1):
-        q *= 1
-    return q
-n = int(input('Введите число'))
-print(asd(n))
-assert asd(3) == 6
-assert asd(4) == 24
--- ДЗ4/home31.py
+++
@@ -1,7 +0,0 @@
-def dqw():
-    p = int(input('Введи символы'))
-    a = []
-    while p != "":
-        a.append(p)
-        p = int(input('Введи символы'))
-    return a
